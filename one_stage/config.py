7322674e3ebbacc248b69dc2efe0410d4391b24c
ndtands <ndtan.hcm@gmail.com>
2022-12-31 06:03:40 -0500

    update config

--- one_stage/config.py
+++ one_stage/config.py
@@ -1,3 +1,5 @@
 BM25_PATH = "/media/Z/NDT/IR/one_stage/bm25"
 WIKI_PATH = "/media/Z/NDT/IR/dataset/wiki/wikipedia_20220620_cleaned_v5.csv"
 TEST_PATH = "/media/Z/NDT/IR/dataset/test/test_ir.csv"
+WEIGHT_MODEL = "/media/Z/NDT/IR/one_stage_with_ranking/training/weight/best_f1.bin"
+MODEL_NAME = "nguyenvulebinh/vi-mrc-base"
